156bc596546fa68d9748d8728fd974e4b02c8c8e
corneliajohnson <corneliaj91@gmail.com>
2021-01-16 18:55:14 -0500

    initial commit

---
+++ Ribboned/Models/Snag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ribboned.Models
+{
+    public class Snag
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Note { get; set; }
+        public int Seconds { get; set; }
+    }
+}
