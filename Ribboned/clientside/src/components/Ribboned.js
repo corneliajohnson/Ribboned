22798e0b3204516495545933c2f8ab5e9fa0950c
corneliajohnson <corneliaj91@gmail.com>
2021-01-17 20:31:05 -0500

    added nav

---
+++ Ribboned/clientside/src/components/Ribboned.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
+
+export const Ribboned = () => (
+  <>
+    <NavBar />
+    <ApplicationViews />
+  </>
+);
