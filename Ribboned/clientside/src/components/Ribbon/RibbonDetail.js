393d4a19fb69e1013352070f0d378c577b549ec7
corneliajohnson <corneliaj91@gmail.com>
2021-01-22 17:54:51 -0500

    added video

--- Ribboned/clientside/src/components/Ribbon/RibbonDetail.js
+++ Ribboned/clientside/src/components/Ribbon/RibbonDetail.js
@@ -1,10 +1,15 @@
 import React from "react";
+//import "../../../node_modules/video-react/dist/video-react";
+import { Player } from "video-react";
 
 export const RibbonDetail = () => {
   return (
     <>
       <div className="container">
         <h1>Ribbon Detail</h1>
+        <Player>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </Player>
       </div>
     </>
   );
