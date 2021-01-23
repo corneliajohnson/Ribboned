335b6a5dfe5f84d7b382bfd3c81b1c8591795917
corneliajohnson <corneliaj91@gmail.com>
2021-01-22 20:48:20 -0500

    added playpause btn

--- Ribboned/clientside/src/components/Ribbon/RibbonDetail.js
+++ Ribboned/clientside/src/components/Ribbon/RibbonDetail.js
@@ -1,15 +1,38 @@
-import React from "react";
-//import "../../../node_modules/video-react/dist/video-react";
-import { Player } from "video-react";
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { Button } from "reactstrap";
 
 export const RibbonDetail = () => {
+  const [state, setState] = useState({
+    playing: false,
+  });
+
+  const { playing } = state;
+
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
+    console.log("hi");
+  };
+
   return (
     <>
-      <div className="container">
-        <h1>Ribbon Detail</h1>
-        <Player>
-          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-        </Player>
+      <div className="container ">
+        <h1 className="text-center">Ribbon Title</h1>
+        <div>
+          <div className="d-flex justify-content-center">
+            <ReactPlayer
+              // muted={true}
+              playing={playing}
+              controls={true}
+              url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            />
+          </div>
+          <div className="text-center m-3">
+            <Button className="btn btn-lg w-50" onClick={handlePlayPause}>
+              Click
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
--- Ribboned/clientside/src/components/Ribboned.js
+++ Ribboned/clientside/src/components/Ribboned.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavBar } from "./nav/NavBar";
 import { ApplicationViews } from "./ApplicationViews";
+import "bootswatch/dist/lux/bootstrap.min.css";
 
 export const Ribboned = () => (
   <>
